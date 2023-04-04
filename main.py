361b9fc229271ac0e25935441c65d81a0acb0371
Amin Mousavi <108234550+emenmousavi@users.noreply.github.com>
2023-04-04 14:42:13 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import dns.resolver
 import argparse
 
+def retrieve_dns_records(domain, record_type=None, record_name=None):
+    try:
+        answers = dns.resolver.query(domain, record_type)
+        if record_name:
+            return [rdata for rdata in answers if record_name == rdata.to_text().split()[0]]
+        else:
+            return list(answers)
+    except dns.resolver.NXDOMAIN:
+        print('Error: Domain name is invalid')
+        return []
+    except dns.resolver.NoAnswer:
+        print('Error: No answer found for the specified record type and name')
+        return []
+    except dns.exception.Timeout:
+        print('Error: Connection timed out')
+        return []
+
 # Parse command line arguments
 parser = argparse.ArgumentParser(description='Check a domain\'s DNS records and DNS Zone provider.')
 parser.add_argument('domain', help='Domain name to check')
@@ -12,17 +29,14 @@ args = parser.parse_args()
 print('\033[1m' + 'LightDNS Checker by Emen Mousavi\n' + '\033[0m')
 
 # Retrieve DNS records
-answers = dns.resolver.query(args.domain, args.type)
+dns_records = retrieve_dns_records(args.domain, args.type, args.name)
 
 # Print DNS records
-if args.name:
-    for rdata in answers:
-        if args.name == rdata.to_text().split()[0]:
-            print(rdata)
-else:
-    for rdata in answers:
+if dns_records:
+    for rdata in dns_records:
         print(rdata)
 
 # Print DNS Zone provider
-ns_answers = dns.resolver.query(args.domain, 'NS')
-print('\nDNS Zone provider:', ns_answers[0])
+dns_zone_providers = retrieve_dns_records(args.domain, 'NS')
+if dns_zone_providers:
+    print('\nDNS Zone provider:', dns_zone_providers[0])
